e0fe82365c3dc82a01432c93314f1984777b1f5f
Ralr26 <98673861+Ralr26@users.noreply.github.com>
2022-02-12 22:21:58 -0600

    Tarea 3

---
+++ 201900163_Tarea3/Casa Cambios/Casa Cambios/Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBoxVenta = New System.Windows.Forms.TextBox()
+        Me.TextBoxCompra = New System.Windows.Forms.TextBox()
+        Me.RadioButtonVenta = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonCompra = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CheckBoxColon = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxEuro = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxPeso = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxDolares = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ButtonSalir = New System.Windows.Forms.Button()
+        Me.ButtonLimpiar = New System.Windows.Forms.Button()
+        Me.ButtonCalcular = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.TextBoxTotalVenta = New System.Windows.Forms.TextBox()
+        Me.TextBoxTotalCompra = New System.Windows.Forms.TextBox()
+        Me.TotalVenta = New System.Windows.Forms.Label()
+        Me.TotalCompra = New System.Windows.Forms.Label()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.TextBoxComisionVenta = New System.Windows.Forms.TextBox()
+        Me.TextBoxComisionCompra = New System.Windows.Forms.TextBox()
+        Me.LbVenta = New System.Windows.Forms.Label()
+        Me.LbCompra = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.IndianRed
+        Me.GroupBox1.Controls.Add(Me.TextBoxVenta)
+        Me.GroupBox1.Controls.Add(Me.TextBoxCompra)
+        Me.GroupBox1.Controls.Add(Me.RadioButtonVenta)
+        Me.GroupBox1.Controls.Add(Me.RadioButtonCompra)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.MediumSpringGreen
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tipo de transacción"
+        '
+        'TextBoxVenta
+        '
+        Me.TextBoxVenta.Location = New System.Drawing.Point(85, 63)
+        Me.TextBoxVenta.Name = "TextBoxVenta"
+        Me.TextBoxVenta.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxVenta.TabIndex = 7
+        '
+        'TextBoxCompra
+        '
+        Me.TextBoxCompra.Location = New System.Drawing.Point(85, 28)
+        Me.TextBoxCompra.Name = "TextBoxCompra"
+        Me.TextBoxCompra.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxCompra.TabIndex = 5
+        '
+        'RadioButtonVenta
+        '
+        Me.RadioButtonVenta.AutoSize = True
+        Me.RadioButtonVenta.Location = New System.Drawing.Point(9, 63)
+        Me.RadioButtonVenta.Name = "RadioButtonVenta"
+        Me.RadioButtonVenta.Size = New System.Drawing.Size(53, 17)
+        Me.RadioButtonVenta.TabIndex = 6
+        Me.RadioButtonVenta.TabStop = True
+        Me.RadioButtonVenta.Text = "Venta"
+        Me.RadioButtonVenta.UseVisualStyleBackColor = True
+        '
+        'RadioButtonCompra
+        '
+        Me.RadioButtonCompra.AutoSize = True
+        Me.RadioButtonCompra.Location = New System.Drawing.Point(9, 28)
+        Me.RadioButtonCompra.Name = "RadioButtonCompra"
+        Me.RadioButtonCompra.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButtonCompra.TabIndex = 5
+        Me.RadioButtonCompra.TabStop = True
+        Me.RadioButtonCompra.Text = "Compra"
+        Me.RadioButtonCompra.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.IndianRed
+        Me.GroupBox2.Controls.Add(Me.CheckBoxColon)
+        Me.GroupBox2.Controls.Add(Me.CheckBoxEuro)
+        Me.GroupBox2.Controls.Add(Me.CheckBoxPeso)
+        Me.GroupBox2.Controls.Add(Me.CheckBoxDolares)
+        Me.GroupBox2.ForeColor = System.Drawing.Color.MediumSpringGreen
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 118)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 140)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Tipo de Moneda"
+        '
+        'CheckBoxColon
+        '
+        Me.CheckBoxColon.AutoSize = True
+        Me.CheckBoxColon.Location = New System.Drawing.Point(25, 117)
+        Me.CheckBoxColon.Name = "CheckBoxColon"
+        Me.CheckBoxColon.Size = New System.Drawing.Size(137, 17)
+        Me.CheckBoxColon.TabIndex = 8
+        Me.CheckBoxColon.Text = "Colones Costarricences"
+        Me.CheckBoxColon.UseVisualStyleBackColor = True
+        '
+        'CheckBoxEuro
+        '
+        Me.CheckBoxEuro.AutoSize = True
+        Me.CheckBoxEuro.Location = New System.Drawing.Point(25, 87)
+        Me.CheckBoxEuro.Name = "CheckBoxEuro"
+        Me.CheckBoxEuro.Size = New System.Drawing.Size(48, 17)
+        Me.CheckBoxEuro.TabIndex = 7
+        Me.CheckBoxEuro.Text = "Euro"
+        Me.CheckBoxEuro.UseVisualStyleBackColor = True
+        '
+        'CheckBoxPeso
+        '
+        Me.CheckBoxPeso.AutoSize = True
+        Me.CheckBoxPeso.Location = New System.Drawing.Point(25, 52)
+        Me.CheckBoxPeso.Name = "CheckBoxPeso"
+        Me.CheckBoxPeso.Size = New System.Drawing.Size(99, 17)
+        Me.CheckBoxPeso.TabIndex = 6
+        Me.CheckBoxPeso.Text = "Peso Mexicano"
+        Me.CheckBoxPeso.UseVisualStyleBackColor = True
+        '
+        'CheckBoxDolares
+        '
+        Me.CheckBoxDolares.AutoSize = True
+        Me.CheckBoxDolares.Location = New System.Drawing.Point(25, 23)
+        Me.CheckBoxDolares.Name = "CheckBoxDolares"
+        Me.CheckBoxDolares.Size = New System.Drawing.Size(51, 17)
+        Me.CheckBoxDolares.TabIndex = 5
+        Me.CheckBoxDolares.Text = "Dolar"
+        Me.CheckBoxDolares.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.IndianRed
+        Me.GroupBox3.Controls.Add(Me.ButtonSalir)
+        Me.GroupBox3.Controls.Add(Me.ButtonLimpiar)
+        Me.GroupBox3.Controls.Add(Me.ButtonCalcular)
+        Me.GroupBox3.ForeColor = System.Drawing.Color.MediumSpringGreen
+        Me.GroupBox3.Location = New System.Drawing.Point(237, 87)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(200, 115)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Opciones"
+        '
+        'ButtonSalir
+        '
+        Me.ButtonSalir.BackColor = System.Drawing.Color.Black
+        Me.ButtonSalir.Location = New System.Drawing.Point(62, 77)
+        Me.ButtonSalir.Name = "ButtonSalir"
+        Me.ButtonSalir.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonSalir.TabIndex = 2
+        Me.ButtonSalir.Text = "Salir"
+        Me.ButtonSalir.UseVisualStyleBackColor = False
+        '
+        'ButtonLimpiar
+        '
+        Me.ButtonLimpiar.BackColor = System.Drawing.Color.Black
+        Me.ButtonLimpiar.Location = New System.Drawing.Point(62, 48)
+        Me.ButtonLimpiar.Name = "ButtonLimpiar"
+        Me.ButtonLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonLimpiar.TabIndex = 1
+        Me.ButtonLimpiar.Text = "Limpieza"
+        Me.ButtonLimpiar.UseVisualStyleBackColor = False
+        '
+        'ButtonCalcular
+        '
+        Me.ButtonCalcular.BackColor = System.Drawing.Color.Black
+        Me.ButtonCalcular.Location = New System.Drawing.Point(62, 19)
+        Me.ButtonCalcular.Name = "ButtonCalcular"
+        Me.ButtonCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonCalcular.TabIndex = 0
+        Me.ButtonCalcular.Text = "Calcular"
+        Me.ButtonCalcular.UseVisualStyleBackColor = False
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.BackColor = System.Drawing.Color.IndianRed
+        Me.GroupBox4.Controls.Add(Me.TextBoxTotalVenta)
+        Me.GroupBox4.Controls.Add(Me.TextBoxTotalCompra)
+        Me.GroupBox4.Controls.Add(Me.TotalVenta)
+        Me.GroupBox4.Controls.Add(Me.TotalCompra)
+        Me.GroupBox4.ForeColor = System.Drawing.Color.MediumSpringGreen
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 285)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(200, 98)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Total "
+        '
+        'TextBoxTotalVenta
+        '
+        Me.TextBoxTotalVenta.Location = New System.Drawing.Point(85, 58)
+        Me.TextBoxTotalVenta.Name = "TextBoxTotalVenta"
+        Me.TextBoxTotalVenta.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxTotalVenta.TabIndex = 9
+        '
+        'TextBoxTotalCompra
+        '
+        Me.TextBoxTotalCompra.Location = New System.Drawing.Point(85, 25)
+        Me.TextBoxTotalCompra.Name = "TextBoxTotalCompra"
+        Me.TextBoxTotalCompra.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxTotalCompra.TabIndex = 8
+        '
+        'TotalVenta
+        '
+        Me.TotalVenta.AutoSize = True
+        Me.TotalVenta.Location = New System.Drawing.Point(6, 58)
+        Me.TotalVenta.Name = "TotalVenta"
+        Me.TotalVenta.Size = New System.Drawing.Size(62, 13)
+        Me.TotalVenta.TabIndex = 6
+        Me.TotalVenta.Text = "Total Venta"
+        '
+        'TotalCompra
+        '
+        Me.TotalCompra.AutoSize = True
+        Me.TotalCompra.Location = New System.Drawing.Point(6, 25)
+        Me.TotalCompra.Name = "TotalCompra"
+        Me.TotalCompra.Size = New System.Drawing.Size(70, 13)
+        Me.TotalCompra.TabIndex = 5
+        Me.TotalCompra.Text = "Total Compra"
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.BackColor = System.Drawing.Color.IndianRed
+        Me.GroupBox5.Controls.Add(Me.TextBoxComisionVenta)
+        Me.GroupBox5.Controls.Add(Me.TextBoxComisionCompra)
+        Me.GroupBox5.Controls.Add(Me.LbVenta)
+        Me.GroupBox5.Controls.Add(Me.LbCompra)
+        Me.GroupBox5.ForeColor = System.Drawing.Color.MediumSpringGreen
+        Me.GroupBox5.Location = New System.Drawing.Point(237, 285)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(200, 98)
+        Me.GroupBox5.TabIndex = 4
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Total de Comisión"
+        '
+        'TextBoxComisionVenta
+        '
+        Me.TextBoxComisionVenta.Location = New System.Drawing.Point(94, 61)
+        Me.TextBoxComisionVenta.Name = "TextBoxComisionVenta"
+        Me.TextBoxComisionVenta.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxComisionVenta.TabIndex = 10
+        '
+        'TextBoxComisionCompra
+        '
+        Me.TextBoxComisionCompra.Location = New System.Drawing.Point(94, 25)
+        Me.TextBoxComisionCompra.Name = "TextBoxComisionCompra"
+        Me.TextBoxComisionCompra.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxComisionCompra.TabIndex = 9
+        '
+        'LbVenta
+        '
+        Me.LbVenta.AutoSize = True
+        Me.LbVenta.Location = New System.Drawing.Point(6, 61)
+        Me.LbVenta.Name = "LbVenta"
+        Me.LbVenta.Size = New System.Drawing.Size(83, 13)
+        Me.LbVenta.TabIndex = 7
+        Me.LbVenta.Text = "Comision Venta "
+        '
+        'LbCompra
+        '
+        Me.LbCompra.AutoSize = True
+        Me.LbCompra.Location = New System.Drawing.Point(6, 28)
+        Me.LbCompra.Name = "LbCompra"
+        Me.LbCompra.Size = New System.Drawing.Size(88, 13)
+        Me.LbCompra.TabIndex = 6
+        Me.LbCompra.Text = "Comision Compra"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._16c134e4361328bad1a5b27098fa9111
+        Me.ClientSize = New System.Drawing.Size(462, 407)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBoxVenta As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxCompra As System.Windows.Forms.TextBox
+    Friend WithEvents RadioButtonVenta As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButtonCompra As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBoxColon As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxEuro As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxPeso As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxDolares As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents ButtonSalir As System.Windows.Forms.Button
+    Friend WithEvents ButtonLimpiar As System.Windows.Forms.Button
+    Friend WithEvents ButtonCalcular As System.Windows.Forms.Button
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBoxTotalVenta As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxTotalCompra As System.Windows.Forms.TextBox
+    Friend WithEvents TotalVenta As System.Windows.Forms.Label
+    Friend WithEvents TotalCompra As System.Windows.Forms.Label
+    Friend WithEvents GroupBox5 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBoxComisionVenta As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxComisionCompra As System.Windows.Forms.TextBox
+    Friend WithEvents LbVenta As System.Windows.Forms.Label
+    Friend WithEvents LbCompra As System.Windows.Forms.Label
+
+End Class
---
+++ 201900163_Tarea3/Casa Cambios/Casa Cambios/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class Form1
+    Public Property CambioColon As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Const CambioDolares As Double = 7.69
+        Const CambioPeso As Double = 0.38
+        Const CambioEuro As Double = 8.79
+        Const CambioColon As Double = 0.012
+        Const ComisionCompra As Double = 2.5 / 100
+        Const ComisionVenta As Double = 3 / 100
+
+        Dim CompraDolares As Double = 0
+        Dim CompraPeso As Double = 0
+        Dim CompraEuro As Double = 0
+        Dim CompraColon As Double = 0
+
+        Dim VentaDolares As Double = 0
+        Dim VentaPeso As Double = 0
+        Dim VentaEuro As Double = 0
+        Dim VentaColon As Double = 0
+
+        Dim CompraComision As Double = 0
+        Dim VentaComision As Double = 0
+    End Sub
+
+    Private Sub RadioButtonCompra_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButtonCompra.CheckedChanged
+        If RadioButtonCompra.Checked = True Then
+            TextBoxCompra.Enabled = True
+        Else
+            TextBoxCompra.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub RadioButtonVenta_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButtonVenta.CheckedChanged
+        If RadioButtonVenta.Checked = True Then
+            TextBoxVenta.Enabled = True
+        Else
+            TextBoxVenta.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub ButtonCalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonCalcular.Click
+        If (RadioButtonCompra.Checked) And (CheckBoxDolares.Checked) Then
+            CompraDolar = TextBoxCompra.Text * CambioDolares
+            TotalCompra.Text = CompraDolares
+        End If
+
+        If (RadioButtonCompra.Checked) And (CheckBoxPeso.Checked) Then
+            CompraPeso = TextBoxCompra.Text * CambioPeso
+            TotalCompra.Text = CompraPeso
+        End If
+
+        If (RadioButtonCompra.Checked) And (CheckBoxEuro.Checked) Then
+            CompraEuro = TextBoxCompra.Text * CambioEuro
+            TotalCompra.Text = CompraEuro
+        End If
+
+        If (RadioButtonCompra.Checked) And (CheckBoxColones.Checked) Then
+            Dim CompraColon As Object
+            CompraColon = TextBoxCompra.Text * CambioColon
+            TotalCompra.Text = CompraColon
+        End If
+
+        If (RadioButtonCompra.Checked) Then
+            CompraComision = TotalCompra.Text * ComisionCompra
+            TextBoxComisionCompra = CompraComision
+        End If
+
+        If (RadioButtonVenta.Checked) And (CheckBoxDolares.Checked) Then
+            VentaDolares = TextBoxVenta.Text * CambioDolares
+            TotalVenta.Text = VentaDolares
+        End If
+
+        If (RadioButtonVenta.Checked) And (CheckBoxPeso.Checked) Then
+            VentaPeso = TextBoxVenta.Text * CambioPeso
+            TotalVenta.Text = VentaPeso
+        End If
+
+        If (RadioButtonVenta.Checked) And (CheckBoxEuro.Checked) Then
+            VentaEuro = TextBoxVenta.Text * CambioEuro
+            TotalVenta.Text = VentaEuro
+        End If
+
+        If (RadioButtonVenta.Checked) And (CheckBoxColones.Checked) Then
+            VentaColon = TextBoxVenta.Text * CambioColon
+            TotalVenta.Text = VentaColon
+        End If
+
+        If (RadioButtonVenta.Checked) Then
+            VentaComision = TotalVenta.Text * ComisionVenta
+            TextBoxComisionVenta = VentaComision
+        End If
+
+
+    End Sub
+
+    Private Function VentaDolares() As String
+        Throw New NotImplementedException()
+    End Function
+    End Sub
+
+    Private Sub ButtonLimpiar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonLimpiar.Click
+        TextBoxCompra.Clear()
+        TextBoxVenta.Clear()
+        TotalCompra.clear()
+        TotalVenta.Clear()
+        TextBoxComisionCompra.Clear()
+        TextBoxComisionVenta.Clear()
+
+    End Sub
+
+
+    Private Sub ButtonSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonSalir.Click
+        If (MsgBox("¿DESEA SALIR?", vbQuestion + vbYesNo, "MENSAJE DE SALIDA") = vbYes) Then
+            End
+        End If
+    End Sub
+    End Sub
+End Class
