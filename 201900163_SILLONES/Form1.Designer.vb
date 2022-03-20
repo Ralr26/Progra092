0bc5db4e08facba13959f0107810edb5ed6426c0
Ralr26 <98673861+Ralr26@users.noreply.github.com>
2022-03-19 20:47:18 -0600

    Tarea Preparatoria 2

---
+++ 201900163_SILLONES/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CbTamaño = New System.Windows.Forms.ComboBox()
+        Me.dgv_Valores = New System.Windows.Forms.DataGridView()
+        Me.No = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Tamaño = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Tela = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Costo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Venta = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GuardarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LimpiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DatosDeEntradaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VectoresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.CbTela = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgv_Valores, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.MediumBlue
+        Me.GroupBox2.Controls.Add(Me.CbTamaño)
+        Me.GroupBox2.Font = New System.Drawing.Font("MS PGothic", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.GroupBox2.Location = New System.Drawing.Point(36, 83)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(176, 159)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Tamaño"
+        '
+        'CbTamaño
+        '
+        Me.CbTamaño.BackColor = System.Drawing.Color.DarkCyan
+        Me.CbTamaño.ForeColor = System.Drawing.SystemColors.Window
+        Me.CbTamaño.FormattingEnabled = True
+        Me.CbTamaño.Items.AddRange(New Object() {"Sofa", "Individual", "Doble"})
+        Me.CbTamaño.Location = New System.Drawing.Point(18, 80)
+        Me.CbTamaño.Name = "CbTamaño"
+        Me.CbTamaño.Size = New System.Drawing.Size(141, 29)
+        Me.CbTamaño.TabIndex = 0
+        '
+        'dgv_Valores
+        '
+        Me.dgv_Valores.BackgroundColor = System.Drawing.Color.MediumBlue
+        Me.dgv_Valores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_Valores.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.No, Me.Tamaño, Me.Tela, Me.Costo, Me.Venta})
+        Me.dgv_Valores.Location = New System.Drawing.Point(232, 83)
+        Me.dgv_Valores.Name = "dgv_Valores"
+        Me.dgv_Valores.Size = New System.Drawing.Size(687, 90)
+        Me.dgv_Valores.TabIndex = 3
+        '
+        'No
+        '
+        Me.No.HeaderText = "No."
+        Me.No.Name = "No"
+        '
+        'Tamaño
+        '
+        Me.Tamaño.HeaderText = "Tamaño"
+        Me.Tamaño.Name = "Tamaño"
+        '
+        'Tela
+        '
+        Me.Tela.HeaderText = "Tela"
+        Me.Tela.Name = "Tela"
+        '
+        'Costo
+        '
+        Me.Costo.HeaderText = "Costo"
+        Me.Costo.Name = "Costo"
+        '
+        'Venta
+        '
+        Me.Venta.HeaderText = "Venta"
+        Me.Venta.Name = "Venta"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.DarkCyan
+        Me.MenuStrip1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GuardarToolStripMenuItem, Me.LimpiarToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(931, 27)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'GuardarToolStripMenuItem
+        '
+        Me.GuardarToolStripMenuItem.Name = "GuardarToolStripMenuItem"
+        Me.GuardarToolStripMenuItem.Size = New System.Drawing.Size(87, 23)
+        Me.GuardarToolStripMenuItem.Text = "Guardar"
+        '
+        'LimpiarToolStripMenuItem
+        '
+        Me.LimpiarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DatosDeEntradaToolStripMenuItem, Me.VectoresToolStripMenuItem})
+        Me.LimpiarToolStripMenuItem.Name = "LimpiarToolStripMenuItem"
+        Me.LimpiarToolStripMenuItem.Size = New System.Drawing.Size(82, 23)
+        Me.LimpiarToolStripMenuItem.Text = "Limpiar"
+        '
+        'DatosDeEntradaToolStripMenuItem
+        '
+        Me.DatosDeEntradaToolStripMenuItem.Name = "DatosDeEntradaToolStripMenuItem"
+        Me.DatosDeEntradaToolStripMenuItem.Size = New System.Drawing.Size(231, 24)
+        Me.DatosDeEntradaToolStripMenuItem.Text = "Datos"
+        '
+        'VectoresToolStripMenuItem
+        '
+        Me.VectoresToolStripMenuItem.Name = "VectoresToolStripMenuItem"
+        Me.VectoresToolStripMenuItem.Size = New System.Drawing.Size(231, 24)
+        Me.VectoresToolStripMenuItem.Text = "Valores de la tabla"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(58, 23)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.MediumBlue
+        Me.GroupBox3.Controls.Add(Me.CbTela)
+        Me.GroupBox3.Font = New System.Drawing.Font("MS PGothic", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.GroupBox3.Location = New System.Drawing.Point(36, 269)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(176, 159)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Tela"
+        '
+        'CbTela
+        '
+        Me.CbTela.BackColor = System.Drawing.Color.DarkCyan
+        Me.CbTela.ForeColor = System.Drawing.SystemColors.Window
+        Me.CbTela.FormattingEnabled = True
+        Me.CbTela.Items.AddRange(New Object() {"Cuero", "Cuerina"})
+        Me.CbTela.Location = New System.Drawing.Point(18, 80)
+        Me.CbTela.Name = "CbTela"
+        Me.CbTela.Size = New System.Drawing.Size(141, 29)
+        Me.CbTela.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.ClientSize = New System.Drawing.Size(931, 470)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.dgv_Valores)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.dgv_Valores, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents CbTamaño As ComboBox
+    Friend WithEvents dgv_Valores As DataGridView
+    Friend WithEvents No As DataGridViewTextBoxColumn
+    Friend WithEvents Tamaño As DataGridViewTextBoxColumn
+    Friend WithEvents Tela As DataGridViewTextBoxColumn
+    Friend WithEvents Costo As DataGridViewTextBoxColumn
+    Friend WithEvents Venta As DataGridViewTextBoxColumn
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents GuardarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LimpiarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DatosDeEntradaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VectoresToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents CbTela As ComboBox
+End Class
---
+++ 201900163_SILLONES/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+
+
+    Public Indice As String
+    Public Matriz(10, Indice) As String
+
+
+    Private Sub GuardarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GuardarToolStripMenuItem.Click
+
+        Dim TamSofa = 250.99
+        Dim TamIndiv = 150.99
+        Dim TamDoble = 200.99
+
+        Dim CueroSofa = 600
+        Dim CueroIndv = 262.5
+        Dim CueroDoble = 450
+
+        Dim CuerinaSofa = 367.92
+        Dim CuerinaIndiv = 160.965
+        Dim CuerinaDobe = 275.94
+
+        If Indice < 8 Then
+
+
+            Matriz(0, Indice) = CbTamaño.Text
+            Matriz(1, Indice) = CbTela.Text
+
+
+            Dim costo As Double
+            Dim PrecioVenta As Double
+
+            If (CbTamaño.Text = "Sofa" And CbTela.Text = "Cuero") Then
+                costo = TamSofa + CueroSofa
+
+            ElseIf (CbTamaño.Text = "Sofa" And CbTela.Text = "Cuerina") Then
+
+                costo = TamSofa + CuerinaSofa
+
+            ElseIf (CbTamaño.Text = "Individual" And CbTela.Text = "Cuero") Then
+
+                costo = TamIndiv + CueroIndv
+
+            ElseIf (CbTamaño.Text = "Individual" And CbTela.Text = "Cuerina") Then
+
+                costo = TamIndiv + CuerinaIndiv
+
+            ElseIf (CbTamaño.Text = "Doble" And CbTela.Text = "Cuero") Then
+                costo = TamDoble + CueroDoble
+
+            ElseIf (CbTamaño.Text = "Doble" And CbTela.Text = "Cuerina") Then
+                costo = TamDoble + CuerinaDobe
+
+            End If
+
+            PrecioVenta = costo + costo * 0.65
+
+            Matriz(2, Indice) = costo
+            Matriz(3, Indice) = PrecioVenta
+
+
+
+            dgv_Valores.Rows.Add(Indice + 1, Matriz(0, Indice), Matriz(1, Indice), Matriz(2, Indice), Matriz(3, Indice))
+            Indice = 1 + Indice
+
+            If CbTamaño.Text = "" Then
+                MsgBox("Ingrese un tamaño")
+            End If
+
+            If CbTela.Text = "" Then
+                MsgBox("Ingrese un Tipo de tela")
+            End If
+
+        Else
+            MsgBox("Maximo Número de Sillones alcanzado")
+
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        If (MsgBox("¿DESEA SALIR?", vbQuestion + vbYesNo, "MENSAJE DE SALIDA") = vbYes) Then
+            Me.Close()
+            End
+        End If
+    End Sub
+
+    Private Sub DatosDeEntradaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DatosDeEntradaToolStripMenuItem.Click
+        CbTamaño.Text = ""
+        CbTela.Text = ""
+    End Sub
+
+    Private Sub VectoresToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VectoresToolStripMenuItem.Click
+        Indice = 0
+        dgv_Valores.Rows.Clear()
+    End Sub
+End Class
